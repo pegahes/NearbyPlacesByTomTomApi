17e2d1752734777923452f9a95828038a919b266
p.esmaily <p.esmaily@kian.com>
2022-05-27 02:45:25 +0430

    created basics for fragment UI

---
+++ app/src/main/java/com/example/sotoontest/features/detail/FragmentPlacesDetail.kt
@@ -0,0 +1,7 @@
+package com.example.sotoontest.features.detail
+
+import androidx.fragment.app.Fragment
+import com.example.sotoontest.R
+
+class FragmentPlacesDetail : Fragment(R.layout.fragment_places_detail){
+}
---
+++ app/src/main/java/com/example/sotoontest/features/list/FragmentPlacesList.kt
@@ -0,0 +1,7 @@
+package com.example.sotoontest.features.list
+
+import androidx.fragment.app.Fragment
+import com.example.sotoontest.R
+
+class FragmentPlacesList : Fragment(R.layout.fragment_places_list) {
+}
